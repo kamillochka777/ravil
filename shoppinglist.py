35528a10e11dbfd7553824e0da103d9054a37bda
Камилла Хусаинова <kamillahusainova957@gmail.com>
2026-06-04 02:00:03 +0300

    создан метод add_recipe

--- shoppinglist.py
+++ shoppinglist.py
@@ -1,3 +1,10 @@
 class ShoppingList():
     def __init__(self):
         self._items = []
+    def add_recipe(self, recipe: Recipe, portions: float):
+        if portions <= 0:
+            raise ValueError('Количество порций должно быть положительным')
+        sc_recipe = recipe.scale(portions)
+        for ingredient in sc_recipe._ingredients:
+            self._items.append((ingredient, recipe.title))
+        
