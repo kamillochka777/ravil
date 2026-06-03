0b3151920e0a513b6c9a0ba7ec9f023f346dbc2d
Камилла Хусаинова <kamillahusainova957@gmail.com>
2026-06-04 00:52:51 +0300

    создан атрибут quantity

--- ingredient.py
+++ ingredient.py
@@ -1,1 +1,15 @@
-print("Tigran")
+class Ingredient:
+    def __init__(self, name, quantity, unit):
+        self.name = name
+        self.quantity = quantity
+        self.unit = unit
+    @property
+    def quantity(self):
+        return self._quantity
+    @quantity.setter
+    def quantity(self, f_quantity):
+        f_quantity = float(f_quantity)
+        if f_quantity <= 0:
+            raise ValueError('Колтчество должно быть положительным')
+        self._quantity = f_quantity
+        
