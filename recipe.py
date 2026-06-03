21328449ef94ef8ebe9097c1fbbf71a3ab9b1543
Камилла Хусаинова <kamillahusainova957@gmail.com>
2026-06-04 01:22:01 +0300

    создан метод add_ingredient

--- recipe.py
+++ recipe.py
@@ -4,3 +4,10 @@ class Recipe():
         self._ingredients = []
         for ingr in ingredients:
             self.add_ingredient(ingr)
+    def add_ingredient(self, ingredient: Ingredient):
+        for ingr in self._ingredients:
+            if ingr.name == ingredient.name and ingr.unit == ingredient.unit:
+                ingr.quantity = ingr.quantity + ingredient.quantity
+                return
+            self._ingredients.append(ingredient)
+    
