6c245b0925d462e61e7dfb0ce8b41cf0e1c322c7
Камилла Хусаинова <kamillahusainova957@gmail.com>
2026-06-04 03:15:48 +0300

    создан переопределенный метод scale

--- dietaryrecipe.py
+++ dietaryrecipe.py
@@ -2,3 +2,7 @@ class DietaryRecipe(Recipe):
     def __init__(self, title, diet_type, ingredients = None):
         super().__init__(title, ingredients if ingredients is not None else [])
         self.diet_type = diet_type
+    def scale(self, ratio: float):
+        scaled = super().scale(ratio)
+        return DietaryRecipe(scaled.title, self.diet_type, scaled._ingredients)
+    
